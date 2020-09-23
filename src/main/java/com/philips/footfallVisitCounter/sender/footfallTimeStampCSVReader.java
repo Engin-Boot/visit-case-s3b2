050aeef0aef7e3f7db5f8d0f28eeed3e710fe189
balaram249 <69622981+balaram249@users.noreply.github.com>
2020-09-23 09:43:33 +0530

    Create footfallTimeStampCSVReader.java

---
+++ src/main/java/com/philips/footfallVisitCounter/sender/footfallTimeStampCSVReader.java
@@ -0,0 +1,32 @@
+package com.philips.footfallVisitCounter.sender;
+import java.io.BufferedReader;  
+import java.io.FileReader;  
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List; 
+public class footFallTimeStampCSVReader {
+	
+	
+	public List<String> readFootfallDataFromCSVFile()
+	{  
+	String line = " "; 
+	List<String> footfallData = new ArrayList<String>();
+	
+	try   
+	{  
+	  
+	BufferedReader br = new BufferedReader(new FileReader("footfallData.csv"));  
+	while ((line = br.readLine()) != null)    
+	{  
+	footfallData.add(line);
+			
+	}  
+	}   
+	catch (IOException e)   
+	{  
+	e.printStackTrace();  
+	}  
+	return footfallData;
+	}  
+
+}
