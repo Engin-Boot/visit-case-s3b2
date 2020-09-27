ea398d2a8a39af1137b540a43749219d23cb129c
balaram1prac <balaram1uchiha@gmail.com>
2020-09-27 15:36:08 +0530

    average daily footfall for week added to aggregateutiltites

--- src/main/java/receiver/aggregateUtiltiesForFootFallData.java
+++ src/main/java/receiver/aggregateUtiltiesForFootFallData.java
@@ -1,5 +1,5 @@
 package receiver;
-
+import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -23,7 +23,43 @@ public static void getpeakDailyFootFall(Map<Integer,Map<Integer,Integer>> mapOfF
 	}
 	System.out.println(peakDailyFootFall);
 	}
-
+public static void getAverageDailyFootFallInAWeek(Map<Integer,Map<Integer,Integer>> mapOfFootFallData)
+{
+  //int peakDailyFootFall=0;
+  Map<Integer,Integer> dailyFootFallOverAMonth=new TreeMap<Integer,Integer>();
+  int temp;
+  Set<Map.Entry<Integer,Map<Integer,Integer>>> s = mapOfFootFallData.entrySet();
+  Set<Map.Entry<Integer, Integer>> innerS;
+  for (Map.Entry<Integer,Map<Integer,Integer>> it: s)
+  {
+    innerS=it.getValue().entrySet();
+    temp=0;
+    for(Map.Entry<Integer, Integer> innerIterator:innerS)
+    {
+      temp=temp+innerIterator.getValue();
+    }
+    dailyFootFallOverAMonth.put(it.getKey(),temp);
+    //if(temp>peakDailyFootFall) peakDailyFootFall=temp;
+  }
+  System.out.println(dailyFootFallOverAMonth);
+  float averageDailyFootFallInAWeek[]= {0,0,0,0,0};
+  System.out.println(averageDailyFootFallInAWeek);
+  innerS=dailyFootFallOverAMonth.entrySet();
+  for(Map.Entry<Integer, Integer> it:innerS)
+  {
+	  System.out.println(it.getKey()/7);
+	  System.out.println(it.getValue());
+	  averageDailyFootFallInAWeek[it.getKey()/7]=averageDailyFootFallInAWeek[it.getKey()/7]+it.getValue();
+  }
+  for(int j=0;j<5;j++)
+  {
+	  averageDailyFootFallInAWeek[j]=averageDailyFootFallInAWeek[j]/7;
+  }
+  System.out.println(Arrays.toString(averageDailyFootFallInAWeek));
+ 
+  //averageDailyFootFallInAWeek
+  //System.out.println(peakDailyFootFall);
+}
 public static void main(String args[])
 {
 	List <String> FootFallData=new ArrayList <String>();
@@ -35,6 +71,6 @@ public static void main(String args[])
 	FootFallData.add("2020-09-18,12:30:35");
 	System.out.println(csvUtilitiesForFootFallData.getTreeMapOfFootFallData(FootFallData));
 	aggregateUtiltiesForFootFallData.getpeakDailyFootFall(csvUtilitiesForFootFallData.getTreeMapOfFootFallData(FootFallData));
-	//aggregateUtiltiesForFootFallData.getAverageDailyFootFallInAWeek(csvUtilitiesForFootFallData.getTreeMapOfFootFallData(FootFallData));
+	aggregateUtiltiesForFootFallData.getAverageDailyFootFallInAWeek(csvUtilitiesForFootFallData.getTreeMapOfFootFallData(FootFallData));
 }
 }
