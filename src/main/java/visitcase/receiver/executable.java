7734d6ea6770519b203e38f177ca4b41f2f9746b
balaram1prac <balaram1uchiha@gmail.com>
2020-10-02 17:24:41 +0530

    trying reading from system

---
+++ src/main/java/visitcase/receiver/executable.java
@@ -0,0 +1,21 @@
+package visitcase.receiver;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.InputStreamReader; 
+public class executable {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		//F f =new F();
+		//f.setV(new csv());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+			String s=reader.readLine();
+		
+		System.out.println("s");}
+		//catch(IOException e) {System.out.println("ok error");}
+		
+	}
+
+
