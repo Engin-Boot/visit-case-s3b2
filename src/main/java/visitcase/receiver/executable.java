728fb30a1dfc0321e377f1a6c45bcd3fb3efc977
balaram1prac <balaram1uchiha@gmail.com>
2020-10-02 19:33:52 +0530

    prototype added

--- src/main/java/visitcase/receiver/executable.java
+++ src/main/java/visitcase/receiver/executable.java
@@ -6,15 +6,18 @@ import java.io.InputStreamReader;
 public class executable {
 
 	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		//F f =new F();
-		//f.setV(new csv());
+		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		
-			String s=reader.readLine();
-		
-		System.out.println("s");}
-		//catch(IOException e) {System.out.println("ok error");}
+		String s=reader.readLine();
+		System.out.println(s);
+		HLM hlm=new HLM();
+		hlm.setV(new csv());
+		String listofrecords=hlm.getstring();
+	Map <> m=	hlm.getmap(listofrecords);
+	hlm.avgdailyfootfalls();
+	hlm.avhhourlyfootfalls();
+	hlm.peakfootfalls();
+	}
 		
 	}
 
---
+++ src/main/java/visitcase/receiver/utilitiesForFootFallData.java
@@ -0,0 +1,5 @@
+package visitcase.receiver;
+
+public class utilitiesForFootFallData {
+
+}
